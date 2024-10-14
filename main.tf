3e603d284c3341b695a5584b5c48c3cff5661759
root <root@ip-172-31-37-243.ec2.internal>
2024-10-14 14:15:14 +0000

    main.tf

---
+++ main.tf
@@ -0,0 +1,34 @@
+resource "aws_launch_configuration" "web_server_as" {
+    image_id           = "ami-0195204d5dce06d99"
+    instance_type = "t2.micro"
+    key_name = "final"
+    security_groups = [aws_security_group.web_server.id]
+}
+   
+
+
+  resource "aws_elb" "web_server_lb"{
+     name = "web-server-lb"
+     security_groups = [aws_security_group.web_server.id]
+     subnets = ["subnet-0c42873ceef71b275", "subnet-01b9af9baf0cb7e1a"]
+     listener {
+      instance_port     = 8000
+      instance_protocol = "http"
+      lb_port           = 80
+      lb_protocol       = "http"
+    }
+    tags = {
+      Name = "terraform-elb"
+    }
+  }
+resource "aws_autoscaling_group" "web_server_asg" {
+    name                 = "web-server-asg"
+    launch_configuration = aws_launch_configuration.web_server_as.name
+    min_size             = 1
+    max_size             = 3
+    desired_capacity     = 2
+    health_check_type    = "EC2"
+    load_balancers       = [aws_elb.web_server_lb.name]
+    availability_zones    = ["us-east-1b", "us-east-1d"] 
+    
+  }
